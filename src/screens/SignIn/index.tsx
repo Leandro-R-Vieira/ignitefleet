37f0b999081563ca32b66692c9405479995ab1d7
Leandro Rezende Vieira <leandrorvieira22@yahoo.com>
2023-09-10 18:50:00 -0300

    Project structure

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,10 +1,20 @@
-import { View } from 'react-native';
-import { styles } from './styles';
+import { Container, Title, Slogan } from './styles';
+import backgroundImg from '../../assets/background.png';
+import { Button } from '../../components/Button';
 
 export function SignIn() {
   return (
-    <View style={styles.container}>
-      
-    </View>
+   <Container source={backgroundImg}>    
+    <Title>
+      Ignite Fleet
+    </Title>
+
+    <Slogan>
+      Gestão de uso de veículos
+    </Slogan>
+
+    <Button title="Entrar com Google"/>
+
+   </Container>
   );
 }
